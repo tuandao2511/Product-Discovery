cf58add9a6e62587b6eab9e8127673a927b6e1a7
tuandao2511 <anhdt251196@gmail.com>
2019-08-25 01:30:22 +0700

    init project base

--- app/src/main/java/com/sunasterisk/tuandao/productdiscovery/screen/search/SearchActivity.kt
+++ app/src/main/java/com/sunasterisk/tuandao/productdiscovery/screen/search/SearchActivity.kt
@@ -1,18 +1,28 @@
 package com.sunasterisk.tuandao.productdiscovery.screen.search
 
 import android.os.Bundle
+import com.sunasterisk.tuandao.productdiscovery.databinding.ActivitySearchBinding
+import com.sunasterisk.tuandao.productdiscovery.screen.base.activity.BaseActivityDataBinding
+import androidx.databinding.DataBindingUtil.setContentView
 import com.sunasterisk.tuandao.productdiscovery.R
 import com.sunasterisk.tuandao.productdiscovery.data.repository.DetailRepository
+import com.sunasterisk.tuandao.productdiscovery.databinding.ActivitySearchBinding
+import com.sunasterisk.tuandao.productdiscovery.screen.base.activity.BaseActivityDataBinding
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
-class SearchActivity : DaggerAppCompatActivity() {
-
-    @Inject
-    lateinit var repository: DetailRepository
+class SearchActivity : BaseActivityDataBinding<ActivitySearchBinding, SearchViewModel>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+    }
+
+    override fun retrieveViewOrRestoreState(savedInstanceState: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getLayoutRes(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
